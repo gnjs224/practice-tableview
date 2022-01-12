5e647dd5db911f11ca3c43e6836a5053836ca9a6
ben224 <ben224@naver.com>
2022-01-12 14:25:58 +0900

    make simple tableView

--- practice-tableview/ViewController.swift
+++ practice-tableview/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier: String = "cell"
+    let korean: [String] = ["가","나","다","라","마","바","사","아","자"]
+    let english: [String] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return korean.count
+        case 1:
+            return english.count
+        default:
+            return 0
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        let text: String = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "한글" : "영어"
+    }
 }
 
